3e75be88ddf2dd4ad06f5e50fa1e0fdd4f28fe2f
Zhang-Hexiang <zhanghexiang2017@163.com>
2022-05-21 12:29:03 +0200

    High Score Board Function step1

--- src/main/java/com/example/uuii/HighScoreViewController.java
+++ src/main/java/com/example/uuii/HighScoreViewController.java
@@ -1,27 +1,111 @@
 package com.example.uuii;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class HighScoreViewController {
     @FXML
-    private TableView highScoreTable;
+    private TableView<HighScoreViewController> highScoreTable;
     @FXML
-    private TableColumn rankCol;
+    private TableColumn<HighScoreViewController, Number> rankCol;
     @FXML
-    private TableColumn playerIDCol;
+    private TableColumn<HighScoreViewController, String> playerIDCol;
     @FXML
-    private TableColumn stepsCol;
+    private TableColumn<HighScoreViewController, Number> stepsCol;
     @FXML
-    private TableColumn startCol;
+    private TableColumn<HighScoreViewController, String> startCol;
     @FXML
-    private TableColumn endCol;
+    private TableColumn<HighScoreViewController, String> endCol;
+    @FXML
+    private TableColumn<HighScoreViewController, String> totalCol;
+
+    private String playerID;
+    private int playerRank;
+    private int playerSteps;
+    private Long playerStartTime;
+    private Long playerEndTime;
+    private Long playerTotalTime;
+    private String playerTotalTimeString;
+
+    public HighScoreViewController(PlayerInfo playerInfo){
+        this.playerID = playerInfo.getID();
+        this.playerSteps = playerInfo.getCount();
+        this.playerStartTime = playerInfo.getStartDate();
+        this.playerEndTime = playerInfo.getEndDate();
+        this.playerTotalTime = playerEndTime - playerStartTime;
+    }
+
+    public HighScoreViewController(String id, int steps, Long start, Long end){
+        this.playerID = id;
+        this.playerSteps = steps;
+        this.playerStartTime = start;
+        this.playerEndTime = end;
+        this.playerTotalTime = playerEndTime - playerStartTime;
+    }
+
+    public HighScoreViewController(int rank,String id, int steps, Long start, Long end){
+        this.playerRank = rank;
+        this.playerID = id;
+        this.playerSteps = steps;
+        this.playerStartTime = start;
+        this.playerEndTime = end;
+        this.playerTotalTime = playerEndTime - playerStartTime;
+    }
+    public HighScoreViewController(int rank, String id, int steps, String totalTime){
+        this.playerRank = rank;
+        this.playerID = id;
+        this.playerSteps = steps;
+        this.playerTotalTimeString = totalTime;
+    }
+
+    public void setPlayerID(String playerID) {
+        this.playerID = playerID;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+    public void setPlayerRank(int rank){
+        this.playerRank = rank;
+    }
+
+    public int getPlayerRank() {
+        return playerRank;
+    }
+    public void setPlayerStartTime(Long start){
+        this.playerStartTime = start;
+    }
+
+    public Long getPlayerStartTime() {
+        return playerStartTime;
+    }
+
+    public void setPlayerEndTime(Long playerEndTime) {
+        this.playerEndTime = playerEndTime;
+    }
+
+    public Long getPlayerEndTime() {
+        return playerEndTime;
+    }
+
+    public void setPlayerTotalTime(Long playerTotalTime) {
+        this.playerTotalTime = playerTotalTime;
+    }
+
+    public Long getPlayerTotalTime() {
+        return playerTotalTime;
+    }
 
     @FXML
     void initialize(){
-//         rankCol.setText("1");
-//         playerIDCol.setText("asd");
+        ObservableList<HighScoreViewController> lis = FXCollections.observableArrayList();
+        HighScoreViewController player1 = new HighScoreViewController(1,"aa",10,"123");
+        HighScoreViewController player2 = new HighScoreViewController(2,"bb",16,"88888");
+        lis.addAll(player1,player2);
+        highScoreTable.setItems(lis);
 
     }
 }
